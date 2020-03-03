91afe0c471acd2ded5250b3b1ec6283a6a9e8e50
Shaik Sadath <sadath.sa@gmail.com>
2020-03-03 20:21:49 +0530

    v1.0.1

---
+++ login.js
@@ -0,0 +1,12 @@
+function login(email,pass) 
+{
+    if(email=='sadath.sa@gmail.com')
+        {
+            if(pass=='Shaik@123')
+                window.open('main.html', '_blank');
+            else
+                alert("The user name or Password is incorect");
+        }
+        else
+            alert("The user name or Password is incorect");    
+}
